9053cb1e3e99acd327a9ee608d3babbca72b754a
Shravani-14-in <shravanibhoir1408@gmail.com>
2026-03-11 22:35:39 +0530

    Implement bitwise complement function

--- 1009. Complement of Base 10 Integer /Solution.java
+++ 1009. Complement of Base 10 Integer /Solution.java
@@ -1,1 +1,17 @@
-
+class Solution {
+    public int bitwiseComplement(int n) {
+        String x= Integer.toBinaryString(n);
+         char[] arr = x.toCharArray();
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]=='1'){
+                arr[i]='0';
+            }
+           else{
+                 arr[i]='1';
+            }
+        }
+         String result = new String(arr);
+          int num = Integer.parseInt(result, 2);
+         return  num;
+    }
+}
